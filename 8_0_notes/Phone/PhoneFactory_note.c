00f440fb7454efbb6d4dab5a763dc4cd87957cf0
lixuqing <lixuqing@auto-link.com.cn>
2018-09-19 18:19:05 +0800

    忘记保存 apn

--- 8_0_notes/Phone/PhoneFactory_note.c
+++ 8_0_notes/Phone/PhoneFactory_note.c
@@ -55,7 +55,7 @@ public class PhoneFactory {
 		/* public static final String FEATURE_TELEPHONY_EUICC = "android.hardware.telephony.euicc";
 		* hasSystemFeature 的实现再 ApplicationPackageManager
 		*/
-                if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY_EUICC)) {
+                if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY_EUICC)) {	
                     sEuiccController = EuiccController.init(context);
                 }
 
