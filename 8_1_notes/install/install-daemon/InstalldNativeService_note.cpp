87a6f41b998a8c94044622395e03560ff6b6fe8e
lixuqing <lixuqing@auto-link.com.cn>
2018-11-23 18:30:27 +0800

    maps

--- 8_1_notes/install/install-daemon/InstalldNativeService_note.cpp
+++ 8_1_notes/install/install-daemon/InstalldNativeService_note.cpp
@@ -1,3 +1,4 @@
+//		frameworks/native/cmds/installd/InstalldNativeService.cpp
 status_t InstalldNativeService::start() {
     IPCThreadState::self()->disableBackgroundScheduling(true);
     status_t ret = BinderService<InstalldNativeService>::publish();
@@ -9,3 +10,21 @@ status_t InstalldNativeService::start() {
     ps->giveThreadPoolName();
     return android::OK;
 }
+
+
+//	@frameworks/native/libs/binder/include/binder/BinderService.h
+template<typename SERVICE>
+class BinderService
+{
+public:
+    static status_t publish(bool allowIsolated = false) {
+        sp<IServiceManager> sm(defaultServiceManager());
+        return sm->addService(String16(SERVICE::getServiceName()),new SERVICE(), allowIsolated);
+    }
+}
+
+
+static char const* getServiceName() { return "installd"; }
+
+
+//	frameworks/native/cmds/installd/binder/android/os/IInstalld.aidl
