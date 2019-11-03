f6bc6766291d31ca32d3b6c30f0868224dee3f5b
lixuqing <lixuqing2007@126.com>
2019-11-03 23:39:32 +0800

    audioserver

--- android-auto-9-notes/audioserver/AudioFlinger/MixerThread/AudioStreamOutSink/NBAIO_Sink.cpp
+++
@@ -1,9 +0,0 @@
-
-NBAIO_Port::NBAIO_Port(const NBAIO_Format& format) : mNegotiated(false), mFormat(format),
-                                             mFrameSize(Format_frameSize(format)) { }
-
-//  @   frameworks/av/media/libnbaio/NBAIO.cpp
-NBAIO_Sink::NBAIO_Sink(const NBAIO_Format& format = Format_Invalid) : NBAIO_Port(format), mFramesWritten(0)
-{
-
-}
